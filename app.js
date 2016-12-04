35d68d58f139a42a312dc541d9151fb82263e8fe
Don Viszneki <donv@wms2006.com>
2016-12-03 16:52:54 -0800

    queue cmd coming

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var irc = require('irc')
 ,   IO = require('socket.io')
 ,   http = require('http')
 ,   express = require('express')
+,   request = require('request')
+,   jsdom = require('jsdom')
 ;
 
 var ircConfig = {
@@ -15,8 +17,40 @@ var bot = new irc.Client(ircConfig.server, ircConfig.botName, {
 });
 
 bot.addListener('message', (from, to, text, message) => {
-  bot.say(from, 'what?');
+  maybeCommand(from, text);
 });
+
+function maybeCommand(user, cmd)
+{
+  cmd = cmd.split(' ');
+  switch (cmd[0])
+  {
+    case '!queue':
+      queueSong(cmd[1]);
+      break;
+    case '!skip':
+      break;
+    default:
+      bot.say(user, "no such command");
+      return;
+  }
+}
+
+function queueSong(url)
+{
+  /* TODO sanitize URL */
+  jsdom.env(
+    url
+  , []
+  , (err, window) => {
+    if (err) {
+      bot.say(ircConfig.channels[0], "sorry. couldn't queue. there was an error");
+      console.error(err);
+      return;
+    }
+    bot.say(ircConfig.channels[0], 'queued ' + window.document.title);
+  });
+}
 
 var app = express();
 var httpServer = http.Server(app);
